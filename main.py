3c7b125f6a2730fab76e9fd5d3c3addc8a54e6e0
Mark Johnson <mark.jhns.in>
2018-04-10 21:51:59 -0500

    Things render! There are nav links!

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 
-app = Flask(__title__)
+app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
@@ -14,8 +14,7 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    date = db.Column(db.String(20))
-    body = db.Column(db.String(10000))
+    body = db.Column(db.String(5000))
 
     def __init__(self, title, body):
         self.title = title
@@ -27,27 +26,32 @@ def index():
 
     if request.method == 'POST':
         blog_title = request.form['blog']
-        new_blog = blog(blog_title)
+        new_blog =Blog(blog_title)
         db.session.add(new_blog)
         db.session.commit()
 
-    blogs = blog.query.filter_by(completed=False).all()
-    completed_blogs = blog.query.filter_by(completed=True).all()
-    return render_template('todos.html', title="Get It Done!",
-                           blogs=blogs, completed_blogs=completed_blogs)
+    blogs = Blog.query.all()
+    completed_blogs = Blog.query.all()
+    return render_template('blog.html', title="Blogs!", blogs=blogs)
+
+@app.route('/newpost', methods=['POST','GET'])
+def newpost():
+
+    
+
+    return render_template('newpost.html',title='New Post')
 
 
-@app.route('/delete-blog', methods=['POST'])
-def delete_blog():
+#@app.route('/delete-blog', methods=['POST'])
+#def delete_blog():
+#
+#    blog_id = int(request.form['blog-id'])
+#    blog = blog.query.get(blog_id)
+#    return redirect('/')
+#    blog.completed = True
+#    db.session.add(blog)
+#    db.session.commit()
 
-    blog_id = int(request.form['blog-id'])
-    blog = blog.query.get(blog_id)
-    blog.completed = True
-    db.session.add(blog)
-    db.session.commit()
 
-    return redirect('/')
-
-
-if __title__ == '__main__':
+if __name__ == '__main__':
     app.run()
